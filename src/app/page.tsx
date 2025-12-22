e8e90bde3663e01da90271490315f88f999c01bb
mohameddn988 <msideneche@gmail.com>
2025-12-22 16:38:43 +0100

    Comment out unused sections in the homepage layout for future development. This includes ActivitiesSection, TrainingSection, and EventsSection, streamlining the current view.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
     <main className="min-h-screen">
       <HeroSection />
       <AboutSection />
-      <ActivitiesSection />
-      <TrainingSection />
-      <EventsSection />
+      {/* <ActivitiesSection /> */}
+      {/* <TrainingSection /> */}
+      {/* <EventsSection /> */}
     </main>
   );
 }
